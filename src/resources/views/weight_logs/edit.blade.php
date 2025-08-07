a197db7fd0853be2001c5bcbf4e61119f1ab0f5b
Yoshiki2 <dds.quartet_cats@ezweb.ne.jp>
2025-08-07 13:25:54 +0900

    微調整

--- src/resources/views/weight_logs/edit.blade.php
+++ src/resources/views/weight_logs/edit.blade.php
@@ -3,7 +3,7 @@
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/weight_logs.css') }}">
 <link rel="stylesheet" href="{{ asset('css/weightlog_edit.css') }}">
-<!-- FontAwesome CDNはレイアウトで読み込み済なら不要 -->
+<link rel="stylesheet" href="{{ asset('css/weight_log.css') }}">
 @endsection
 
 @section('body-class', 'app-body')
@@ -37,20 +37,20 @@
         @method('PUT')
 
         {{-- 日付 --}}
-        <div class="form-group">
-            <label for="date">日付</label>
-            <input type="date" id="date" name="date" value="{{ old('date', $weightLog->date) }}">
+        <div class="edit-form-group">
+            <label for="date" class="edit-label">日付</label>
+            <input type="date" id="date" name="date" value="{{ old('date', $weightLog->date) }}" class="edit-input">
             @error('date')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
 
         {{-- 体重 --}}
-        <div class="form-group">
-            <label for="weight">体重</label>
-            <div class="input-unit-row">
-                <input type="text" id="weight" name="weight" value="{{ old('weight', $weightLog->weight) }}">
-                <span class="input-unit">kg</span>
+        <div class="edit-form-group">
+            <label for="weight" class="edit-label">体重</label>
+            <div class="edit-input-unit-row">
+                <input type="text" id="weight" name="weight" value="{{ old('weight', $weightLog->weight) }}" class="edit-input">
+                <span class="edit-unit">kg</span>
             </div>
             @error('weight')
                 <div class="error-message">{{ $message }}</div>
@@ -58,11 +58,11 @@
         </div>
 
         {{-- 摂取カロリー --}}
-        <div class="form-group">
-            <label for="meal_calories">摂取カロリー</label>
-            <div class="input-unit-row">
-                <input type="text" id="meal_calories" name="meal_calories" value="{{ old('meal_calories', $weightLog->meal_calories) }}">
-                <span class="input-unit">cal</span>
+        <div class="edit-form-group">
+            <label for="meal_calories" class="edit-label">摂取カロリー</label>
+            <div class="edit-input-unit-row">
+                <input type="text" id="meal_calories" name="meal_calories" value="{{ old('meal_calories', $weightLog->meal_calories) }}" class="edit-input">
+                <span class="edit-unit">cal</span>
             </div>
             @error('meal_calories')
                 <div class="error-message">{{ $message }}</div>
@@ -70,27 +70,27 @@
         </div>
 
         {{-- 運動時間 --}}
-        <div class="form-group">
-            <label for="exercise_time">運動時間</label>
-            <input type="time" id="exercise_time" name="exercise_time" value="{{ old('exercise_time', $weightLog->exercise_time ?? '00:00') }}">
+        <div class="edit-form-group">
+            <label for="exercise_time" class="edit-label">運動時間</label>
+            <input type="time" id="exercise_time" name="exercise_time" value="{{ old('exercise_time', $weightLog->exercise_time ?? '00:00') }}" class="edit-input">
             @error('exercise_time')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
 
         {{-- 運動内容 --}}
-        <div class="form-group">
-            <label for="exercise_content">運動内容</label>
-            <textarea id="exercise_content" name="exercise_content" rows="2" placeholder="運動内容を追加">{{ old('exercise_content', $weightLog->exercise_content) }}</textarea>
+        <div class="edit-form-group">
+            <label for="exercise_content" class="edit-label">運動内容</label>
+            <textarea id="exercise_content" name="exercise_content" rows="2" placeholder="運動内容を追加" class="edit-textarea">{{ old('exercise_content', $weightLog->exercise_content) }}</textarea>
             @error('exercise_content')
                 <div class="error-message">{{ $message }}</div>
             @enderror
         </div>
 
         {{-- ボタン群 --}}
-        <div class="modal-buttons">
-            <a href="{{ route('weight_logs.index') }}" class="btn-back">戻る</a>
-            <button type="submit" class="btn-register">更新</button>
+        <div class="edit-btn-row">
+            <a href="{{ route('weight_logs.index') }}" class="edit-btn edit-btn-gray">戻る</a>
+            <button type="submit" class="edit-btn edit-btn-grad">更新</button>
         </div>
     </form>
 
@@ -98,7 +98,7 @@
     <form action="{{ route('weight_logs.destroy', $weightLog->id) }}" method="POST" class="edit-delete-form" onsubmit="return confirm('本当に削除しますか？');">
         @csrf
         @method('DELETE')
-        <button type="submit" class="btn-delete">
+        <button type="submit" class="edit-btn-delete">
             <i class="fas fa-trash trash-icon"></i>
         </button>
     </form>
